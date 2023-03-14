7ac4a43579a307eee8a4c809af7ee30ebbfdfcf5
Halvor Øverby <halvorov@stud.ntnu.no>
2023-03-14 09:59:19 +0100

    add comment

--- brukerhistorieD.py
+++ brukerhistorieD.py
@@ -1,5 +1,11 @@
 import sqlite3
 
+#Bruker skal kunne søke etter togruter som går mellom en 
+#startstasjon og en sluttstasjon, med utgangspunkt i en dato 
+# og et klokkeslett. Alle ruter den samme dagen og den neste skal 
+# returneres, sortert på tid. 
+# Denne funksjonaliteten skal programmeres.
+
 
 
 con = sqlite3.connect("tog.db")
@@ -8,9 +14,6 @@ startStasjon= input("Startstasjon: ")
 sluttStasjon= input("Sluttstasjon: ")
 dato= input("dato: ")
 klokkeslett= input("klokkeslett: ")
-cursor.execute(f"SELECT TogruteID \
-               FROM TogRute JOIN TogruteForekomst ON TogRute.TogruteID = TogruteForekomst.TogruteID \
-               JOIN RuteInnom ON  RuteInnom.TogruteID = TogRute.TogruteID\
-               JOIN Jernbanestasjon ON Jernbanestasjon.Stasjonsnavn = RuteInnom.Stasjonsnavn\
-               WHERE dag='{ukedag}' AND stasjonsnavn='{stasjon} ")
+#denne spørringen er gal
+cursor.execute(f"")
 con.close()
