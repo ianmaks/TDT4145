a9acd14323e992a02e40acf2f2454bc54c4ba753
Halvor Øverby <halvorov@stud.ntnu.no>
2023-03-15 13:37:24 +0100

    fix brukerhistore E

--- BrukerhistorerProgging/brukerhistorieE.py
+++ BrukerhistorerProgging/brukerhistorieE.py
@@ -1,6 +1,14 @@
 #En bruker skal kunne registrere seg i kunderegisteret.
 import sqlite3
+from uuid import uuid4
+navn=input("Legg inn navn: ")
+epost=input("Legg inn epost: ")
+mobilnummer=input("Legg inn mobilnummer: ")
+kundenummer=uuid4()
+
 con = sqlite3.connect("tog.db")
 cursor = con.cursor()
-cursor.execute(f"")
+cursor.execute(f"INSERT INTO Kunde \
+                VALUES ({kundenummer},{navn} ,{epost} ,{mobilnummer});") 
+#må vi her legge inn feil om den finnes? dumt med random kundenummer?
 con.close()
