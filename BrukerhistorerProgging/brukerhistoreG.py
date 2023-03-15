50efa1600af1703684de72cc19605b3b5de27a21
Halvor Øverby <halvorov@stud.ntnu.no>
2023-03-15 13:44:15 +0100

    add folder

--- BrukerhistorerProgging/brukerhistoreG.py
+++ BrukerhistorerProgging/brukerhistoreG.py
@@ -1,4 +1,6 @@
-#Registrerte kunder skal kunne finne ledige billetter for en oppgitt strekning på en ønsket togrute og kjøpe de billettene hen ønsker. Denne funksjonaliteten skal programmeres.
+#Registrerte kunder skal kunne finne ledige billetter for en oppgitt strekning på en ønsket togrute 
+#og kjøpe de billettene hen ønsker.
+#
 #Pass på at dere bare selger ledige plasser
 import sqlite3
 con = sqlite3.connect("tog.db")
