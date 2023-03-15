69a464168c49b28a23e9cfa225c65a7ccbc733dd
Halvor Øverby <halvorov@stud.ntnu.no>
2023-03-15 14:04:48 +0100

    startet work on brukerhistore H

--- BrukerhistorerProgging/brukerhistoreH.py
+++ BrukerhistorerProgging/brukerhistoreH.py
@@ -1,6 +1,8 @@
 #For en bruker skal man kunne finne all informasjon om de kjøpene hen har gjort for fremtidige
 #reiser. Denne funksjonaliteten skal programmeres.
 import sqlite3
+
+uuid=input("Legg inn uuid:")
 con = sqlite3.connect("tog.db")
 cursor = con.cursor()
 cursor.execute(f"")
