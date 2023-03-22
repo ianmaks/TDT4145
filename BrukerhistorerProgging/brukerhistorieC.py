d610947a8bc0aaa0090d1def3a514b65bcd36131
Halvor Øverby <halvorov@stud.ntnu.no>
2023-03-22 09:09:51 +0100

    update

--- BrukerhistorerProgging/brukerhistorieC.py
+++ BrukerhistorerProgging/brukerhistorieC.py
@@ -6,11 +6,14 @@
 import sqlite3
 con = sqlite3.connect("python/TDT4145_prosjekt/sql/tog.db")
 cursor = con.cursor()
-stasjon = input("Hvilken stasjon vil du ha togruter for?").lower()
-ukedag = input("Hvilken ukedag ønsker du å sjekke?").lower()
+stasjon = input("Hvilken stasjon vil du ha togruter for?")
+ukedag = input("Hvilken ukedag ønsker du å sjekke?")
+
 cursor.execute(f"Select TogRuteID from TogRute \
                 Natural Join TogruteForekomst \
                 Natural join RuteInnom \
                 Natural join Jernbanestasjon\
                 WHERE TogruteForekomst.Ukedag=='{ukedag}' AND Jernbanestasjon.StasjonsNavn=='{stasjon}';")
+results = cursor.fetchall()
+print(results)
 con.close()
