06cc65ec5c45e9cacd6ef274499db14c9761a5d5
Halvor Øverby <halvorov@stud.ntnu.no>
2023-03-22 15:26:42 +0100

    dis be wak

--- BrukerhistorerProgging/brukerhistorieD.py
+++ BrukerhistorerProgging/brukerhistorieD.py
@@ -7,14 +7,14 @@ import sqlite3
 import datetime
 
 def ukedag(dato):
-    dager = ['mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag', 'søndag']
+    dager = ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag']
 
     dateObj= datetime.datetime.strptime(dato, '%d.%m.%Y')
     ukedagsnummer = dateObj.weekday()
     return dager[ukedagsnummer]
 
 def nesteukedag(dato):
-    dager = ['mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag', 'søndag','mandag']
+    dager = ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag','Mandag']
 
     dateObj= datetime.datetime.strptime(dato, '%d.%m.%Y')
     ukedagsnummer = dateObj.weekday()+1
@@ -25,14 +25,14 @@ con = sqlite3.connect("sql/tog.db")
 startStasjon= input("Startstasjon: ")
 sluttStasjon= input("Sluttstasjon: ")
 dato= input("dato f eks.(31.03.2023): ")
-klokkeslett= input("klokkeslett: ")
+klokkeslett= input("klokkeslett f eks. (07:49:00): ")
 
 cursor = con.cursor()
-cursor.execute(f"Select Togrute.TogruteID, AvgangsTid from Togrute\
-                 join TogruteForekomst ON (TogruteForekomst.TogruteID = TogRute.TogruteID) where \
-                 StartStasjon == '{startStasjon}' and EndeStasjon == '{sluttStasjon}' \
-                 and (Ukedag == '{ukedag(dato)}' OR Ukedag == '{nesteukedag(dato)}') \
-                 order by AvgangsTid;")
+cursor.execute(f"Select Togrute.TogruteID, TogruteForekomst.Ukedag ,Togrute.AvgangsTid from Togrute\
+                join TogruteForekomst ON (TogruteForekomst.TogruteID = TogRute.TogruteID) where \
+                StartStasjon == '{startStasjon}' and EndeStasjon == '{sluttStasjon}' \
+                and ((Ukedag == '{ukedag(dato)}' AND AvgangsTid >= '{klokkeslett}') OR Ukedag == '{nesteukedag(dato)}') \
+                order by AvgangsTid;")
 results = cursor.fetchall()
 print(results)
 con.close()
