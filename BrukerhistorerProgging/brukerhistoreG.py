b0854b6e75490baf59b0fcf99ca0bd79a0b06000
Ian <ianma@stud.ntnu.no>
2023-03-23 14:11:16 +0100

    Oppdatert brukerhistorie G, ikke ferdig

--- BrukerhistorerProgging/brukerhistoreG.py
+++ BrukerhistorerProgging/brukerhistoreG.py
@@ -7,7 +7,7 @@ import sqlite3
 kundenummer=input("Legg inn kundenummer: ")
 print("Velg togrute: \n (1) Trondheim-Bodø-dagtog \n (2) Trondheim-Bodø-nattog \n (3) Mo i Rana-Trondheim-morgentog")
 togruter = ['Trondheim-Bodø-dagtog', 'Trondheim-Bodø-nattog', 'Mo i Rana-Trondheim-morgentog']
-togrute = togruter[int(input())]
+togrute = togruter[int(input("Togrute: "))]
 
 start=input("Startstasjon: ")
 slutt=input("Sluttstasjon: ")
@@ -15,11 +15,12 @@ slutt=input("Sluttstasjon: ")
 con = sqlite3.connect("sql/tog.db")
 cursor = con.cursor()
 
-#cursor.execute(f"select TogRute.TogruteID as Rutenavn, TogruteForekomst.Ukedag as Dag from TogRute join TogruteForekomst where TogRute.TogruteID = TogruteForekomst.TogruteID")
+#cursor.execute(f"Insert into Billett (BillettID, Ordrenummer, DelstrekningID, VognNavn) values ()")
+cursor.execute(f"SELECT Distinct Delstrekning.DelstrekningID, TogruteForekomst.ForekomstID, TogruteForekomst.Ukedag, Billett.BillettID from Billett join HarPlass on Billett.BillettID = HarPlass.BillettID join TogruteForekomst on HarPlass.ForekomstID = TogruteForekomst.ForekomstID join TogRute on TogruteForekomst.TogruteID = TogRute.TogruteID join Delstrekning on Delstrekning.DelstrekningID = Billett.DelstrekningID join RuteInnom on TogRute.TogruteID = RuteInnom.TogruteID where (Delstrekning.StartStasjon = '{start}' and Delstrekning.Endestasjon = '{slutt}') or (RuteInnom.Stasjonsnavn =  '{start}') or (RuteInnom.Stasjonsnavn =  '{slutt}')")
 
-#cursor.execute(f"select Delstrekning.DelstrekningID as ID, Delstrekning.StartStasjon as Start, Delstrekning.Endestasjon as Ende, BestårAv.BaneStrekningNavn as BaneNavn from BestårAv join Delstrekning where BestårAv.DelstrekningID = Delstrekning.DelstrekningID and Delstrekning.StartStasjon = '{start}' and Delstrekning.Endestasjon = '{slutt}'")
 
-cursor.execute(f"select TogruteForekomst.ForekomstID from Delstrekning join BestårAv join Banestrekning join TogRute join TogruteForekomst")
+
+
 
 #kjope=input("angi hvilken du ønsker å kjøpe")
 
