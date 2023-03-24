febd92cdcad0de1a7e745895dc8584825f705d9a
Ian <ianma@stud.ntnu.no>
2023-03-24 15:18:28 +0100

    Oppdatert brukerhistorie G

--- BrukerhistorerProgging/brukerhistoreG.py
+++ BrukerhistorerProgging/brukerhistoreG.py
@@ -3,6 +3,38 @@
 #
 #Pass på at dere bare selger ledige plasser
 import sqlite3
+import itertools
+
+
+def check_avail(checks):
+    cursor.execute(f"""
+    
+    WITH Orders
+	AS (SELECT DISTINCT Delstrekning.DelstrekningID, HarPlass.Plasser
+	FROM   Billett
+    JOIN HarPlass on Billett.BillettID = HarPlass.BillettID
+    JOIN TogruteForekomst on HarPlass.ForekomstID = TogruteForekomst.ForekomstID 
+    JOIN TogRute on TogruteForekomst.TogruteID = TogRute.TogruteID 
+    JOIN Delstrekning on Delstrekning.DelstrekningID = Billett.DelstrekningID 
+    LEFT JOIN StrekningInnom on Delstrekning.DelstrekningID = StrekningInnom.DelstrekningID
+    where (Delstrekning.StartStasjon = '{checks[0]}')
+    OR (Delstrekning.Endestasjon = '{checks[1]}') 
+    OR (StrekningInnom.Stasjonsnavn =  '{checks[0]}') 
+    OR (StrekningInnom.Stasjonsnavn = '{checks[1]}'))
+    SELECT SUM(Plasser)       
+    FROM   Orders
+
+    """)
+    
+    results = cursor.fetchall()
+
+    for i in results:
+        print(checks[0], checks[1])
+        print(i)
+    print("")
+
+con = sqlite3.connect("sql/tog.db")
+cursor = con.cursor()
 
 kundenummer=input("Legg inn kundenummer: ")
 print("Velg togrute: \n (1) Trondheim-Bodø-dagtog \n (2) Trondheim-Bodø-nattog \n (3) Mo i Rana-Trondheim-morgentog")
@@ -12,13 +44,37 @@ togrute = togruter[int(input("Togrute: "))]
 start=input("Startstasjon: ")
 slutt=input("Sluttstasjon: ")
 
-con = sqlite3.connect("sql/tog.db")
-cursor = con.cursor()
+
 
 #cursor.execute(f"Insert into Billett (BillettID, Ordrenummer, DelstrekningID, VognNavn) values ()")
-cursor.execute(f"SELECT Distinct Delstrekning.DelstrekningID, TogruteForekomst.ForekomstID, TogruteForekomst.Ukedag, Billett.BillettID from Billett join HarPlass on Billett.BillettID = HarPlass.BillettID join TogruteForekomst on HarPlass.ForekomstID = TogruteForekomst.ForekomstID join TogRute on TogruteForekomst.TogruteID = TogRute.TogruteID join Delstrekning on Delstrekning.DelstrekningID = Billett.DelstrekningID join RuteInnom on TogRute.TogruteID = RuteInnom.TogruteID where (Delstrekning.StartStasjon = '{start}' and Delstrekning.Endestasjon = '{slutt}') or (RuteInnom.Stasjonsnavn =  '{start}') or (RuteInnom.Stasjonsnavn =  '{slutt}')")
+
+if togrute == 'Mo i Rana-Trondheim-morgentog':
+    stasjoner = ['Mo i Rana', 'Mosjøen', 'Steinkjer', 'Trondheim']
+    check_start = stasjoner.index(f"{start}")
+    check_slutt = stasjoner.index(f"{slutt}")
+    checks = list(itertools.combinations(stasjoner[check_start:check_slutt+1], 2))
+
+else:
+    stasjoner = ["Trondheim", "Steinkjer", "Mosjøen", "Mo i Rana", "Fauske", "Bodø"]
+    check_start = stasjoner.index(f"{start}")
+    check_slutt = stasjoner.index(f"{slutt}")
+    checks = list(itertools.combinations(stasjoner[check_start:check_slutt+1], 2))
+
+for i in checks:
+    check_avail(i)
 
 
+
+
+
+#cursor.execute(f"SELECT distinct TogRute.Startstasjon,TogRute.Endestasjon, RI1.Stasjonsnavn, RI2.Stasjonsnavn  from TogruteForekomst join TogRute on TogruteForekomst.TogruteID = TogRute.TogruteID join RuteInnom as RI1 on TogruteForekomst.TogruteID = RI1.TogruteID join RuteInnom as RI2 on TogruteForekomst.TogruteID = RI2.TogruteID join Delstrekning where (Delstrekning.StartStasjon = RI1.Stasjonsnavn and Delstrekning.Endestasjon = RI2.Stasjonsnavn)")
+#               (Delstrekning.StartStasjon = TogRute.Startstasjon and Delstrekning.Endestasjon = RuteInnom.Stasjonsnavn) or (Delstrekning.StartStasjon = RuteInnom.Stasjonsnavn and Delstrekning.Endestasjon = TogRute.Endestasjon) or (Delstrekning.StartStasjon = RuteInnom.Stasjonsnavn and Delstrekning.Endestasjon = RuteInnom.Stasjonsnavn)")
+#Delstrekning.DelstrekningID,  TogruteForekomst.ForekomstID, TogruteForekomst.Ukedag
+#on 
+# results = cursor.fetchall()
+
+# for i in results:
+#     print(i)
 
 
 
@@ -26,7 +82,5 @@ cursor.execute(f"SELECT Distinct Delstrekning.DelstrekningID, TogruteForekomst.F
 
 #cursor.execute(f"")
 
-results = cursor.fetchall()
-for i in results:
-    print(i)
+
 con.close()
